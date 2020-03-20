50585f535d59d6430a45d5edcf5cb9d26d422e8f
maxim van den eede <maximvde@gmail.com>
2020-03-20 15:08:50 +0100

    Initial commit

---
+++ EFTHighlightDetector/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Management;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Threading;
+
+namespace EFTHighlightDetector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Highlight Detector";
+            var highlightPath = Path.Combine(Path.GetTempPath(), @"Highlights/Escape From Tarkov");
+            var tarkovConfigPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Escape from Tarkov/shared.ini");
+
+            if (!NvidiaCheck())
+            {
+                Console.WriteLine("It appears that no Nvidia cards were found, although, this could be a false positive.\nAre you sure your card is Nvidia Highlights compatible? (Y/N)");
+                Console.WriteLine();
+                Console.Write("-> ");
+                var key = Console.ReadKey();
+                if (key.KeyChar.ToString().ToLower() != "y") Environment.Exit(0);
+            }
+
+            Console.Clear();
+
+            PrepHighlightDir(highlightPath);
+
+            PrepTarkovConfig(tarkovConfigPath);
+
+            var listThread = new Thread(new ParameterizedThreadStart(Listener));
+            listThread.Start(highlightPath);
+        }
+        private static bool NvidiaCheck()
+        {
+            ManagementObjectSearcher objvide = new ManagementObjectSearcher("select * from Win32_VideoController");
+            var gpuInf = new List<ManagementBaseObject>();
+
+            bool cardFound = false;
+            foreach (ManagementObject obj in objvide.Get())
+                if (obj["Name"].ToString().ToLower().Contains("nvidia")) cardFound = true;
+
+            return cardFound;
+        }
+        private static void PrepHighlightDir(string path)
+        {
+            if (!Directory.Exists(path)) Directory.CreateDirectory(path);
+            else if (Directory.GetFiles(path).Length > 0)
+            {
+                Directory.Delete(path);
+                Directory.CreateDirectory(path);
+            }
+        }
+        private static void PrepTarkovConfig(string confPath)
+        {
+            if (File.Exists(confPath))
+            {
+                string confPlain;
+                using (StreamReader confReader = new StreamReader(confPath))
+                {
+                    confPlain = confReader.ReadToEnd();
+                    confReader.Close();
+                }
+                JObject confObj = JObject.Parse(confPlain);
+                JProperty nvProp = confObj.Property("NVidiaHighlightsEnabled");
+                bool isEnabled = nvProp.Value.Value<bool>();
+
+                if (!isEnabled)
+                {
+                    Console.Write("Nvidia Highlights is NOT enabled! Enabling... ");
+                    nvProp.Value = true;
+                    string updatedPlain = JsonConvert.SerializeObject(confObj, Formatting.Indented);
+                    confObj = null;
+                    File.Delete(confPath);
+                    using (StreamWriter confWriter = new StreamWriter(confPath))
+                    {
+                        confWriter.WriteLine(updatedPlain);
+                        confWriter.Flush();
+                    }
+                    Console.WriteLine("OK");
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Could not find the tarkov config location!\nPlease make sure you manually enable Nvidia Highlights in the settings");
+                Console.ReadKey();
+            }
+        }
+        private static void Listener(Object highlightPath) 
+        {
+            int actCount = 0;
+            var path = (string)highlightPath;
+            var killPlayer = new System.Media.SoundPlayer(@"audio.wav");
+            var notificationList = new List<string>();
+
+            Console.WriteLine("Listening for activities...");
+            Console.WriteLine();
+            while (true)
+            {
+                var files = Directory.GetFiles(path);
+                if (files.Length > actCount)
+                {
+                    Console.WriteLine("{0} - {1} kills", DateTime.Now.ToString("hh:mm:ss"), files.Length);
+                    killPlayer.Stop();
+                    killPlayer.Play();
+                }
+                else if (files.Length == 0 && actCount != 0)
+                {
+                    Console.Clear();
+                    notificationList.Add(String.Format("{0} - Game finished, recorded {1} activities", DateTime.Now.ToString("hh:mm:ss"), actCount));
+                    foreach (var notification in notificationList) Console.WriteLine(notification);
+                    Console.WriteLine();
+                    Console.WriteLine("Listening for activities...");
+                    Console.WriteLine();
+                }
+                actCount = files.Length;
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ EFTHighlightDetector/models/TarkovConfig.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFTHighlightDetector.models
+{
+    class TarkovConfig
+    {
+        public class Variant
+        {
+            public bool isAxis { get; set; }
+            public List<object> keyCode { get; set; }
+            public string axisName { get; set; }
+            public bool positiveAxis { get; set; }
+            public double deadZone { get; set; }
+            public double sensitivity { get; set; }
+        }
+
+        public class KeyBinding
+        {
+            public string keyName { get; set; }
+            public List<Variant> variants { get; set; }
+            public string pressType { get; set; }
+            public bool pressAvailable { get; set; }
+        }
+
+        public class Positive
+        {
+            public bool isAxis { get; set; }
+            public List<object> keyCode { get; set; }
+            public string axisName { get; set; }
+            public bool positiveAxis { get; set; }
+            public double deadZone { get; set; }
+            public double sensitivity { get; set; }
+        }
+
+        public class Negative
+        {
+            public bool isAxis { get; set; }
+            public List<object> keyCode { get; set; }
+            public string axisName { get; set; }
+            public bool positiveAxis { get; set; }
+            public double deadZone { get; set; }
+            public double sensitivity { get; set; }
+        }
+
+        public class Pair
+        {
+            public Positive positive { get; set; }
+            public Negative negative { get; set; }
+        }
+
+        public class AxisBinding
+        {
+            public string axisName { get; set; }
+            public List<Pair> pairs { get; set; }
+            public bool isInverted { get; set; }
+            public double gravity { get; set; }
+            public bool snapToZero { get; set; }
+            public string modificator { get; set; }
+        }
+
+        public class GetBoundItemNames
+        {
+            public string ItemV { get; set; }
+            public string Item1 { get; set; }
+            public string Item2 { get; set; }
+            public string Item3 { get; set; }
+            public string Item4 { get; set; }
+            public string Item5 { get; set; }
+            public string Item6 { get; set; }
+            public string Item7 { get; set; }
+            public string Item8 { get; set; }
+            public string Item9 { get; set; }
+            public string Item10 { get; set; }
+        }
+
+        public class RootObject
+        {
+            public List<KeyBinding> keyBindings { get; set; }
+            public List<AxisBinding> axisBindings { get; set; }
+            public bool StatedInvertedXAxisValue { get; set; }
+            public bool StatedInvertedYAxisValue { get; set; }
+            public bool InvertedXAxis { get; set; }
+            public bool InvertedYAxis { get; set; }
+            public double MouseSensitivity { get; set; }
+            public double MouseAimingSensitivity { get; set; }
+            public double DoubleClickTimeout { get; set; }
+            public double StatedFieldOfView { get; set; }
+            public double StatedHeadbobbing { get; set; }
+            public string Language { get; set; }
+            public bool Subtitles { get; set; }
+            public bool TutorialHints { get; set; }
+            public bool AlwaysShowInterfaceElements { get; set; }
+            public bool DontAllowToAddMe { get; set; }
+            public int AimingDeadzone { get; set; }
+            public int Fov { get; set; }
+            public int Headbobbing { get; set; }
+            public bool NVidiaHighlightsEnabled { get; set; }
+            public bool ClearRAM { get; set; }
+            public bool SetAffinityToLogicalCores { get; set; }
+            public bool Blood { get; set; }
+            public bool BadLanguage { get; set; }
+            public int RagfairLinesCountIndex { get; set; }
+            public string EnvironmentUiType { get; set; }
+            public object CharacterVoiceLanguage { get; set; }
+            public int OverallVolume { get; set; }
+            public int EffectsVolume { get; set; }
+            public int InterfaceVolume { get; set; }
+            public int ChatVolume { get; set; }
+            public int MusicVolume { get; set; }
+            public int VoiceVolume { get; set; }
+            public int HideoutVolume { get; set; }
+            public int MicrophoneSensitivity { get; set; }
+            public bool MuteOtherPlayers { get; set; }
+            public bool MusicOnRaidEnd { get; set; }
+            public GetBoundItemNames GetBoundItemNames { get; set; }
+            public int XInvert { get; set; }
+            public int YInvert { get; set; }
+            public int HideoutVolumeValue { get; set; }
+        }
+    }
+}
